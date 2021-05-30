7b2023c3786c8e129280f2c7418966e470a1918b
murillo.maciel <murillo.maciel@rede.ulbra.br>
2021-05-30 20:11:28 -0300

    styling texts

--- catalin_compose_course/1_simple_text_app/app/src/main/java/com/example/simpletextapp/MainActivity.kt
+++ catalin_compose_course/1_simple_text_app/app/src/main/java/com/example/simpletextapp/MainActivity.kt
@@ -3,36 +3,51 @@ package com.example.simpletextapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.simpletextapp.ui.theme.SimpleTextAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SimpleTextAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            Body()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Body() {
+    GreetingText("Murillo")
+}
+
+@Composable
+fun GreetingText(name: String) {
+    Text(
+        text = "Hello $name!",
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(all = 24.dp)
+            .clickable {  },
+        style = MaterialTheme.typography.caption,
+        fontWeight = FontWeight.Bold
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     SimpleTextAppTheme {
-        Greeting("Android")
+        Body()
     }
 }
