1441780226fe19fe7dabc4ea44bc9c0aa8c799f2
murillo.maciel <murillo.maciel@rede.ulbra.br>
2021-06-20 15:12:50 -0300

    finish core ui elements section

--- catalin_compose_course/2_core_compore_app/app/src/main/java/com/example/corecomposeapp/MainActivity.kt
+++ catalin_compose_course/2_core_compore_app/app/src/main/java/com/example/corecomposeapp/MainActivity.kt
@@ -3,9 +3,16 @@ package com.example.corecomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,12 +25,44 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Body() {
-    Greeting("teste")
+    MainScreen()
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen() {
+    Surface(
+        color = Color.DarkGray,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceAround
+            ) {
+                ColoredSquare(color = Color.Black)
+                ColoredSquare(color = Color.Yellow)
+            }
+            ColoredSquare(color = Color.Magenta)
+            ColoredSquare(color = Color.Red)
+            ColoredSquare(color = Color.Blue)
+        }
+    }
+}
+
+@Composable
+fun ColoredSquare(color: Color, text: String = "text") {
+    Surface(
+        color = color,
+        modifier = Modifier
+            .height(100.dp)
+            .width(100.dp)
+    ) {
+        Text(text = text)
+    }
 }
 
 @Preview(showBackground = true)
