f751cd46c1b46742f3c94af0727f0e804a7a2bfc
murillo.maciel <murillo.maciel@rede.ulbra.br>
2021-06-23 13:21:25 -0300

    Add navigation and passing parameters throught composables

--- catalin_compose_course/4_profile_card_layout/app/src/main/java/com/example/profilecardlayout/MainActivity.kt
+++ catalin_compose_course/4_profile_card_layout/app/src/main/java/com/example/profilecardlayout/MainActivity.kt
@@ -5,23 +5,31 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
 import coil.transform.CircleCropTransformation
 import com.example.profilecardlayout.ui.theme.LightGreen
 import com.example.profilecardlayout.ui.theme.MyTheme
@@ -32,23 +40,54 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyTheme {
-                Body()
+                UsersApplication()
             }
         }
     }
 }
 
 @Composable
-fun Body() {
+fun UsersApplication(profiles: List<UserProfile> = userProfileList) {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "users_list") {
+        // this NavGraphBuilder allow us to control how to build a navGraph
+        composable(route = "users_list") {
+            UserListScreen(profiles, navController)
+        }
+        composable(
+            route = "users_details/{userId}",
+            arguments = listOf(
+                navArgument(name = "userId") {
+                    type = NavType.IntType
+                }
+            )
+        ) { navBackStackEntry ->
+            UserDetailsScreen(
+                userId = navBackStackEntry.arguments!!.getInt("userId", 0),
+                navController = navController
+            )
+        }
+    }
+}
+
+@Composable
+fun UserListScreen(profiles: List<UserProfile>, navController: NavController? = null) {
     Scaffold(
-        topBar = { AppBar() },
+        topBar = { AppBar(
+            icon = Icons.Default.Home,
+            title = "users"
+        ) },
     ) {
         Surface(
             modifier = Modifier.fillMaxSize(),
         ) {
             LazyColumn {
-                items(userProfileList) { item ->
-                    ProfileCard(item)
+                items(profiles) { item ->
+                    ProfileCard(item) {
+                        navController?.navigate(
+                            route = "users_details/${item.id}",
+                        )
+                    }
                 }
             }
         }
@@ -56,28 +95,28 @@ fun Body() {
 }
 
 @Composable
-fun AppBar() {
+fun AppBar(title: String, icon: ImageVector, onBackClick: (() -> Unit)? = null) {
     TopAppBar(
         navigationIcon = {
             Icon(
-                imageVector = Icons.Default.Home,
+                imageVector = icon,
                 contentDescription = "Home",
                 modifier = Modifier.padding(horizontal = 12.dp)
+                    .clickable { onBackClick?.invoke() }
             )
-         },
-        title = { Text("Messaging Users") }
+        },
+        title = { Text(title) },
     )
 }
 
 @Composable
-fun ProfileCard(user: UserProfile) {
+fun ProfileCard(user: UserProfile, onClick: () -> Unit) {
     Card(
         modifier = Modifier
-            .padding(top = 8.dp, bottom = 4.dp, start = 16.dp)
+            .padding(top = 8.dp, bottom = 4.dp, start = 16.dp, end = 16.dp)
             .fillMaxWidth()
-            .wrapContentHeight(
-                align = Alignment.Top
-            ),
+            .wrapContentHeight(align = Alignment.Top)
+            .clickable { onClick() },
         elevation = 8.dp,
         backgroundColor = Color.White
     ) {
@@ -88,18 +127,20 @@ fun ProfileCard(user: UserProfile) {
         ) {
             ProfilePicture(
                 imageUrl = user.imageUrl,
-                userStatus = user.status
+                userStatus = user.status,
+                imageSize = 72.dp
             )
             ProfileContent(
                 userName = user.name,
-                userStatus = user.status
+                userStatus = user.status,
+                alignment = Alignment.Start
             )
         }
     }
 }
 
 @Composable
-fun ProfilePicture(imageUrl: String, userStatus: Boolean) {
+fun ProfilePicture(imageUrl: String, userStatus: Boolean, imageSize: Dp) {
     val imageBorderColor = if (userStatus) MaterialTheme.colors.LightGreen else Color.Red
     Card(
         shape = CircleShape,
@@ -118,18 +159,18 @@ fun ProfilePicture(imageUrl: String, userStatus: Boolean) {
                 }
             ),
             contentDescription = "description",
-            modifier = Modifier.size(72.dp),
+            modifier = Modifier.size(imageSize),
         )
     }
 }
 
 @Composable
-fun ProfileContent(userName: String, userStatus: Boolean) {
-    val statusText = stringResource(id = if (userStatus) R.string.user_online else R.string.user_offline)
+fun ProfileContent(userName: String, userStatus: Boolean, alignment: Alignment.Horizontal) {
+    val statusText =
+        stringResource(id = if (userStatus) R.string.user_online else R.string.user_offline)
     Column(
-        modifier = Modifier
-            .padding(8.dp)
-            .fillMaxWidth()
+        modifier = Modifier.padding(8.dp),
+        horizontalAlignment = alignment
     ) {
         Text(
             text = userName,
@@ -148,6 +189,54 @@ fun ProfileContent(userName: String, userStatus: Boolean) {
 @Composable
 fun DefaultPreview() {
     MyTheme {
-        Body()
+        UserListScreen(userProfileList)
+    }
+}
+
+@Composable
+fun UserDetailsScreen(userId: Int = 0, navController: NavController? = null) {
+    val user = userProfileList.first { userId == it.id }
+    Scaffold(
+        topBar = { AppBar(
+            title = user.name,
+            icon = Icons.Default.ArrowBack,
+            onBackClick = {
+                navController?.navigateUp()
+            }
+        ) },
+    ) {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            UserDetailsBody(user)
+        }
+    }
+}
+
+@Composable
+fun UserDetailsBody(user: UserProfile = userProfileList[0]) {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        ProfilePicture(
+            imageUrl = user.imageUrl,
+            userStatus = user.status,
+            imageSize = 240.dp
+        )
+        ProfileContent(
+            userName = user.name,
+            userStatus = user.status,
+            alignment = Alignment.CenterHorizontally,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UserDetailsScreenPreview() {
+    MyTheme {
+        UserDetailsScreen()
     }
 }
